c16a078c602f4314208263487a21ac0fd7ebfd81
Nataliia Kotelnitska <nkotelnicka@gmail.com>
2022-06-03 00:06:13 +0300

    Update App.js

--- src/components/FeedbackOptions/FeedbackOptions.js
+++ src/components/FeedbackOptions/FeedbackOptions.js
@@ -1,9 +1,7 @@
-// Винеси блок кнопок в компонент <FeedbackOptions options={} onLeaveFeedback={}>.
 import PropTypes from 'prop-types';
 import s from './Button.module.css';
 
 export default function FeedbackOptions({ options, onButtonClick }) {
-  console.log(options);
   return (
     <div className={s.container}>
       {Object.keys(options).map(option => {
@@ -15,8 +13,6 @@ export default function FeedbackOptions({ options, onButtonClick }) {
             className={s.button}
             onClick={e => {
               onButtonClick(e.currentTarget.name);
-              // console.log(e.target);
-              // return e.currentTarget.name;
             }}
           >
             {option}
@@ -27,7 +23,7 @@ export default function FeedbackOptions({ options, onButtonClick }) {
   );
 }
 
-// FeedbackOptions.propTypes = {
-//   options: PropTypes.object(PropTypes.string.isRequired),
-//   onButtonClick: PropTypes.func.isRequired,
-// };
+FeedbackOptions.propTypes = {
+  options: PropTypes.object,
+  onButtonClick: PropTypes.func.isRequired,
+};
--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,6 +1,4 @@
-// Винеси відображення статистики в окремий компонент <Statistics good={} neutral={} bad={} total={} positiveercentage={}>.
-
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 import s from './Statistics.module.css';
 
 export default function Statistic({
@@ -10,24 +8,21 @@ export default function Statistic({
   total,
   positivePercentage,
 }) {
-  console.log(good);
   return (
     <div className={s.statisticsWrapper}>
-      {/* <div> */}
       <p className={s.statisticItem}>Good: {good}</p>
       <p className={s.statisticItem}>Neutral: {neutral}</p>
       <p className={s.statisticItem}>Bad: {bad}</p>
       <p className={s.statisticItem}>Total: {total}</p>
       <p className={s.statisticItem}>Positive feedback: {positivePercentage}</p>
-      {/* </div> */}
     </div>
   );
 }
 
-// Statistic.propTypes = {
-//   good: PropTypes.number.isRequired,
-//   neutral: PropTypes.number.isRequired,
-//   bad: PropTypes.number.isRequired,
-//   total: PropTypes.number.isRequired,
-//   positivePercentage: PropTypes.string.isRequired,
-// };
+Statistic.propTypes = {
+  good: PropTypes.number.isRequired,
+  neutral: PropTypes.number.isRequired,
+  bad: PropTypes.number.isRequired,
+  total: PropTypes.number.isRequired,
+  positivePercentage: PropTypes.string.isRequired,
+};
